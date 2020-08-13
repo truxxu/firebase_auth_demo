18bb991af42a75ba065082552b207fb6aca16409
Daniel Trujillo <danielfetrujillo@gmail.com>
2020-08-13 17:06:36 -0500

    config for stack navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
 import React from 'react'
 import { View, Text } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+
+import { Home, Login, Register } from './screens'
+
+const Stack = createStackNavigator();
 
 const App = () => {
   return (
     <NavigationContainer>
-      <View>
-        <Text>Hello World</Text>
-      </View>
+      <Stack.Navigator
+        initialRouteName="Login"
+        screenOptions={{
+          headerTitleAlign: 'center',
+          headerTitleStyle: {
+            fontWeight: 'bold',
+          },
+        }}>
+        <Stack.Screen
+          name="Login"
+          component={Login}
+          options={{ title: 'Login' }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={Register}
+          options={
+            {title: 'Register'},
+            {headerLeft: null}
+          }
+        />
+        <Stack.Screen
+         name="Home"
+         component={Home}
+         options={
+           { title: 'Home' },
+           {headerLeft: null}
+         }
+        />
+      </Stack.Navigator>
     </NavigationContainer>
   )
 }
