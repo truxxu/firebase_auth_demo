93ab4046a3960e071a3d6082c4420b5c920d7d95
Daniel Trujillo <danielfetrujillo@gmail.com>
2020-08-13 18:56:19 -0500

    created home screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {StyleSheet, View, Text, Button} from 'react-native';
 
-const Home = () => {
+import firebase from '../../database/firebase';
+
+const Home = ({navigation}) => {
+  const [error, setError] = useState('');
+  const name = firebase.auth().currentUser.name;
+
+  const signOutUser = async () => {
+    try {
+      await firebase.auth().signOut();
+      navigation.navigate('Login');
+    } catch (err) {
+      setError(err.message);
+    }
+  };
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.textStyle}>Hello, {name}</Text>
+      <Text style={styles.errorText}>{error}</Text>
+      <Button title="Logout" onPress={() => signOutUser()} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 35,
+    backgroundColor: '#fff',
+  },
+  textStyle: {
+    fontSize: 15,
+    marginBottom: 20,
+  },
+  errorText: {
+    color: 'red',
+    marginTop: 25,
+    textAlign: 'center',
+  },
+});
 
 export {Home};
