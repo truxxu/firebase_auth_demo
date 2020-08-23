ec80b7b5ec1edb25e4b60194dbcef4fe6ac1f688
Daniel Trujillo <danielfetrujillo@gmail.com>
2020-08-23 10:51:37 -0500

    added tab navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, {useContext, useEffect} from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import {Context as AuthContext} from './context/AuthContext';
 import {navigationRef} from './RootNavigation';
 import {Spinner} from './atoms';
-import {Home, Login, Register, Welcome} from './screens';
+import {Home, Login, Register, Welcome, Profile} from './screens';
 
 const Stack = createStackNavigator();
+const authStack = () => {
+  return (
+    <Stack.Navigator screenOptions={NavigationOptions}>
+      <Stack.Screen name="Welcome" component={Welcome} />
+      <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="Login" component={Login} />
+    </Stack.Navigator>
+  );
+};
+
+const Tab = createBottomTabNavigator();
+const tabNavigator = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+};
+
+const NavigationOptions = {headerShown: false};
 
 const App = () => {
   const {getUser, state} = useContext(AuthContext);
@@ -25,19 +47,17 @@ const App = () => {
     <NavigationContainer ref={navigationRef}>
       <Stack.Navigator>
         {state.user ? (
-          <Stack.Screen name="Home" component={Home} />
+          <Stack.Screen
+            name="tabNavigator"
+            component={tabNavigator}
+            options={NavigationOptions}
+          />
         ) : (
-          <React.Fragment>
-            <Stack.Screen
-              name="Welcome"
-              component={Welcome}
-              options={{
-                headerShown: false,
-              }}
-            />
-            <Stack.Screen name="Register" component={Register} />
-            <Stack.Screen name="Login" component={Login} />
-          </React.Fragment>
+          <Stack.Screen
+            name="authStack"
+            component={authStack}
+            options={NavigationOptions}
+          />
         )}
       </Stack.Navigator>
     </NavigationContainer>
